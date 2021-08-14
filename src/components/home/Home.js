9c61178d8d8df5ddeb7777a3d4b04a6d1f5cddf6
veera_1505 <veerasaraha@gmail.com>
2021-08-14 13:07:36 +0530

    add home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,59 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import SpacexSvg from '../../spacex.svg'
+
 import './Home.css'
 
 const Home = () => {
+  const [companyInfo, setCompanyInfo] = useState({})
+
+  const { name, founder, founded, summary } = companyInfo
+
+  useEffect(() => {
+    const fetchCompanyInfo = async () => {
+      const response = await axios.get('https://api.spacexdata.com/v3/info')
+
+      setCompanyInfo(response.data)
+    }
+
+    fetchCompanyInfo()
+  }, [])
+
+  console.log(companyInfo)
   return (
-    <div className='home'>
-      <div className='home-wrapper'>
-        <div className='home-top'>
-          <div className='home-logo'>Logo</div>
-          <div className='home-nav-link'>
-            <ul className='home-nav-link-items'>
-              <li>
-                <Link to='/history'>History</Link>
-              </li>
-              <li>
-                <Link to='/launches'>Launches</Link>
-              </li>
-              <li>
-                <Link to='/rockets'>Rockets</Link>
-              </li>
-            </ul>
-          </div>
+    <div className='home-wrapper'>
+      <div className='home-top'>
+        <div className='home-logo'>
+          <img src={SpacexSvg} title='spaceX' alt='spacX-logo' />
         </div>
 
-        <div className='home-bottom'>Something</div>
+        <div className='home-nav-bar'>
+          <ul className='home-nav-bar-links'>
+            <li className='home-nav-bar-item'>
+              <Link to='/history'>History</Link>
+            </li>
+            <li className='home-nav-bar-item'>
+              <Link to='/launches'>Launches</Link>
+            </li>
+            <li className='home-nav-bar-item'>
+              <Link to='/rockets'>Rockets</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='home-bottom'>
+        <section className='home-section-company-info'>
+          <div className='company-info-box-one'>
+            <div className='company-founded-name'>{name}</div>
+          </div>
+          <div className='company-info-box-two'>
+            <div className='company-founded-text'>{founded}</div>
+            <hr className='line' />
+            <div className='company-founded-desc'>{summary}</div>
+          </div>
+        </section>
       </div>
     </div>
   )
